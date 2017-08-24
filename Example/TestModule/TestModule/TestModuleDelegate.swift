fa56f277003cf21d4ff3ce5b3510ff4287cb677d
DreamTracer <jinqiucheng1006@live.cn>
2017-08-24 15:37:10 +0800

    example修改

--- Example/TestModule/TestModule/TestModuleDelegate.swift
+++ Example/TestModule/TestModule/TestModuleDelegate.swift
@@ -7,7 +7,18 @@
 //
 
 import UIKit
+import Roy
 
-class TestModuleDelegate: RoyModule {
+public class TestModuleDelegate: RoyModule {
+    required public init(appScheme: String, host: String) {
+        super.init(appScheme: appScheme, host: host)
 
+        _ = self.addRouter(path: "initializeviewcontroller", viewController: TMViewController.self)
+        _ = RoyGlobal.instance.addRouter(url: URL(string: "Roy://testmodule/hahaha")!, viewController: FirstViewController.self)
+    }
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
 }
