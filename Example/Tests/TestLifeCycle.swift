82aabfe0a14fbcab1e48c1ea126557d1c28f75ed
jinqiucheng <jinqiucheng1006@live.cn>
2018-03-19 23:46:59 +0800

    测试用例

--- Example/Tests/TestLifeCycle.swift
+++ Example/Tests/TestLifeCycle.swift
@@ -7,12 +7,61 @@
 //
 
 import XCTest
+import Roy
+
+
+class LifeCycleClass:NSObject, RoyDelegate {
+    func willConvert(url:String){
+        
+        print(OperationQueue.current ,  #function)
+    }
+    func didConverted(url:String,error:Error?){
+        print(OperationQueue.current ,  #function,error)
+    }
+    func willAdd(url:String){
+        print(OperationQueue.current ,  #function)
+    }
+    func didAdd(url:String,error:Error?){
+        print(OperationQueue.current ,  #function,error)
+    }
+    
+    //route life cycle
+    func willAnalyze(url:URL,param:[String:Any]?){
+        print(OperationQueue.current ,  #function)
+    }
+    func didAnalyzed(url:URL,param:[String:Any]?,error:Error?){
+        print(OperationQueue.current ,  #function,error)
+    }
+    func willValidate(url:URL,param:[String:Any]?){
+        print(OperationQueue.current ,  #function)
+    }
+    func didValidated(url:URL,param:[String:Any]?,error:Error?){
+        print(OperationQueue.current ,  #function,error)
+    }
+    func willFindTask(url:URL,param:[String:Any]?){
+        print(OperationQueue.current ,  #function)
+    }
+    func didFoundTask(url:URL,param:[String:Any]?,error:Error?){
+        print(OperationQueue.current ,  #function,error)
+    }
+    func willRoute(url:URL,param:[String:Any]?){
+        print(OperationQueue.current ,  #function)
+    }
+    func didRouted(url:URL,param:[String:Any]?,error:Error?){
+        print(OperationQueue.current ,  #function,error)
+    }
+}
+
 
 class TestLifeCycle: XCTestCase {
     
+    let queue = OperationQueue()
+    var e : XCTestExpectation?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
     }
     
     override func tearDown() {
@@ -20,9 +69,76 @@ class TestLifeCycle: XCTestCase {
         super.tearDown()
     }
     
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == "operations")
+        {
+            if (0 == self.queue.operations.count)
+            {
+				e!.fulfill()
+            }
+        }
+        else
+        {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
+    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        e = expectation(description: "")
+        self.queue.maxConcurrentOperationCount = 100
+        
+        self.queue.addObserver(self, forKeyPath: "operations", options: NSKeyValueObservingOptions.new, context: nil)
+        let lc = LifeCycleClass()
+        RoyR.regist(delegate: lc)
+        
+        for _ in 0..<5000{
+//            RoyR.global.addRouter(url: "autohome://hahahahaha/aslkdjflsa", task: { (param) -> Any in
+//                return true
+//            }, paramValidator: nil)
+            
+            RoyR.global.addRouter(url: "autohome://hahahahaha/aslkdjflsa", task: { (param) -> Any in
+                return true
+            }, paramValidator: nil, inQueue: self.queue)
+            
+        }
+        
+        wait(for: [e!], timeout: 10)
+        
+        
+        
+//        RoyR.global.addRouter(url: "autohome://hahahahaha/akjsdf", task: { (param) -> Any in
+//            return true
+//        }, paramValidator: nil)
+//
+//        RoyR.global.addRouter(url: "autohome://hahahahaha/akjsdf", task: { (param) -> Any in
+//            return true
+//        }, paramValidator: nil)
+//
+//        RoyR.global.addRouter(url: "autohome://hahahahaha/as", task: { (param) -> Any in
+//            return true
+//        }, paramValidator: nil)
+//
+//        RoyR.global.addRouter(url: "autohome://hahahahaha/asd", task: { (param) -> Any in
+//            return true
+//        }, paramValidator: nil)
+//
+//        RoyR.global.addRouter(url: "autohome://hahahahaha/qieriqwer", task: { (param) -> Any in
+//            return true
+//        }, paramValidator: nil)
+//
+//
+//        RoyR.global.addRouter(url: "autohome://hahahahaha", task: { (param) -> Any in
+//            return true
+//        }, paramValidator: nil)
+        
+        
+        
+        
+        
+        
+        
     }
     
     func testPerformanceExample() {
